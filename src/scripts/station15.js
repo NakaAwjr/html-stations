715626a72f0a3f0dd4a062454fb31f1d07d0ec3e
NakaAwjr <n.sadanana@gmail.com>
2024-02-22 15:55:17 +0900

    Clear 2024/2/22

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,29 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(list) {
+  fullNameList = list.map(buildFullName)
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(fullNameList);
+    }, 3000);
+  }).then(result => {
+    return result
+  });
 }
 
+function buildFullName(data) {
+  array = {
+    id: data.id,
+    full_name: data.family_name + ' ' + data.first_name,
+    first_name: data.first_name,
+    family_name: data.family_name,
+    affiliation: data.affiliation,
+    is_student: data.is_student
+  }
+  return array
+}
